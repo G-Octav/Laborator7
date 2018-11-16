b4a83e27d723abdcca570e34aadd1a93dabd364a
Octavian Gensthaler <Octavian.Gensthaler@endava.com>
2018-11-16 09:30:35 +0200

    Fix conflicts

--- Laborator7.Dal/Manager/IShoppingCartManager.cs
+++ Laborator7.Dal/Manager/IShoppingCartManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Laborator7.Dal.Repository;
 using Laborator7.Dto.ShoppingCart;
 
 namespace Laborator7.Dal.Manager
--- Laborator7.Dal/Repository/IShoppingCartRepository.cs
+++ Laborator7.Dal/Repository/IShoppingCartRepository.cs
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
-=======
-﻿using Laborator7.Domain.ShoppingCart;
-using System;
 using System.Collections.Generic;
-using System.Text;
->>>>>>> 96021209575d520e1a2dd0b4b7af887eb8acb87b
+using Laborator7.Domain.ShoppingCart;
 
 namespace Laborator7.Dal.Repository
 {
     public interface IShoppingCartRepository
     {
         IEnumerable<ShoppingCart> GetAllShoppingCarts();
-<<<<<<< HEAD
-=======
-
         IEnumerable<ShoppingCart> GetAllShoppingCartById();
-
->>>>>>> 96021209575d520e1a2dd0b4b7af887eb8acb87b
     }
 }
